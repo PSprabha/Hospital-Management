557f66847beccc751719b6e9cfdaaa9a953368b0
Sorna Prabhakaran P <154865676+PSprabha@users.noreply.github.com>
2025-06-19 13:34:50 +0530

    Update DbConnection.java

--- careplus-patient-management-system/src/com/zsgs/careplus/repository/db/DbConnection.java
+++ careplus-patient-management-system/src/com/zsgs/careplus/repository/db/DbConnection.java
@@ -7,7 +7,7 @@ public class DbConnection {
     
     private static final String URL = "jdbc:mysql://localhost:3306/careplus";
 	private static final String USER = "root";
-	private static final String PASSWORD = "xxxx";
+	private static final String PASSWORD = "2002";
     public static Connection getConnection() throws Exception {
         return DriverManager.getConnection(URL, USER, PASSWORD);
     }
